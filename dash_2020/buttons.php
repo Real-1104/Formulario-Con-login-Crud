eed3484244da9b9bdc304b7d8e505d6c0164d7c9
unknown <andruthsanchez@gmail.com>
2020-05-07 15:59:52 -0430

    guardas cambios a mi formulario

---
+++ dash_2020/buttons.php
@@ -0,0 +1,9 @@
+<?php require_once "vista/parte_superior.php"?>
+
+<!-- Inicio del contenido principa -->
+<div class="container">
+<h1>Registro Estudiantil</h1>
+</div>
+<!-- Fin del contenido principal  -->
+
+<?php require_once "vista/parte_inferior.php"?>
---
+++ dash_2020/index.php
@@ -0,0 +1,64 @@
+<?php require_once "vista/parte_superior.php"?>
+
+<!-- Inicio del contenido principa -->
+<div class="container">
+<h1>Contenido Principal</h1>
+</div>
+
+
+<?php
+include_once '/bd/conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+$consulta = "SELECT id, nombre, pais, edad FROM personas";
+$resultado = $conexion->prepare($consulta);
+$resultado->execute();
+$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+?>
+    
+<div class="container">
+        <div class="row">
+            <div class="col-lg-12">            
+            <button id="btnNuevo" type="button" class="btn btn-success" data-toggle="modal">Nuevo</button>    
+            </div>    
+        </div>    
+    </div>    
+    <br>  
+    <div class="container">
+        <div class="row">
+                <div class="col-lg-12">
+                    <div class="table-responsive">        
+                        <table id="tablaPersonas" class="table table-striped table-bordered table-condensed" style="width:100%">
+                        <thead class="text-center">
+                            <tr>
+                                <th>Id</th>
+                                <th>Nombre</th>
+                                <th>País</th>                                
+                                <th>Edad</th>  
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php                            
+                            foreach($data as $dat) {                                                        
+                            ?>
+                            <tr>
+                                <td><?php echo $dat['id'] ?></td>
+                                <td><?php echo $dat['nombre'] ?></td>
+                                <td><?php echo $dat['pais'] ?></td>
+                                <td><?php echo $dat['edad'] ?></td>    
+                                <td></td>
+                            </tr>
+                            <?php
+                                }
+                            ?>                                
+                        </tbody>        
+                       </table>                    
+                    </div>
+                </div>
+        </div>  
+    </div>    
+<!-- Fin del contenido principal  -->
+
+<?php require_once "vista/parte_inferior.php"?>
